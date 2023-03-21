75e2c6438b89cb3e32ff257bf17b464e73b27c2d
Mohamed Aouadi <mohamedaouadi@Apples-Mac-mini-2.local>
2023-03-21 23:22:46 +0100

    navigation

---
+++ consultplus/Screens/MainActivityView.swift
@@ -0,0 +1,20 @@
+//
+//  MainActivityView.swift
+//  consultplus
+//
+//  Created by Mohamed Aouadi on 21/3/2023.
+//
+
+import SwiftUI
+
+struct MainActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainActivityView()
+    }
+}
