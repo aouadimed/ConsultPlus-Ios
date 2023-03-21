0937b7f2c438b1d07daffd40e03210c15cb7b7f0
Mohamed Aouadi <mohamedaouadi@Apples-Mac-mini-2.local>
2023-03-21 18:02:03 +0100

    consomation json

---
+++ consultplus/ApiManger/ApiManger.swift
@@ -0,0 +1,12 @@
+//
+//  ApiManger.swift
+//  consultplus
+//
+//  Created by Mohamed Aouadi on 21/3/2023.
+//
+
+import UIKit
+
+class ApiManger: NSObject {
+
+}
---
+++ consultplus/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  consultplus
+//
+//  Created by Mohamed Aouadi on 21/3/2023.
+//
+
+import Foundation
